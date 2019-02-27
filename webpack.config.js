8d2e090fd2ea1a39dd7b93e1bb9a9306a9ee78da
Kristof Soomers <kristof.soomers@bannerconnect.net>
2019-02-27 16:31:01 +0100

    Added ESLint rules of AirBnB

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,11 @@ module.exports = {
             {
                 test:/\.(s*)css$/,
                 use:['style-loader','css-loader', 'sass-loader']
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'eslint-loader']
             }
         ]
     },
