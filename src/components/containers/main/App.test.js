55279e01d852cf5294467d95712696c87a6920f6
Kristof Soomers <kristof.soomers@bannerconnect.net>
2019-02-27 16:15:38 +0100

    Added a basic test with Jest and Enzyme

---
+++ src/components/containers/main/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import App from './App';
+
+configure({ adapter: new Adapter() });
+
+
+describe('First React component test with Enzyme', () => {
+    it('renders without crashing', () => {
+        shallow(<App />);
+    });
+});
