a132df8f29a9b48ae7b28f4555d17bfa6fa00098
Kristof Soomers <kristof.soomers@bannerconnect.net>
2019-02-28 14:43:29 +0100

    Webpack - Config file fully set up for ReactApp Base

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,41 @@
 const Dotenv = require('dotenv-webpack');
+
 const env = process.env.NODE_ENV;
 
 module.exports = {
-    entry: './src/index.js',
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            },
-            {
-                test:/\.(s*)css$/,
-                use:['style-loader','css-loader', 'sass-loader']
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader', 'eslint-loader']
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: './dist'
-    },
-    plugins: [
-        new Dotenv({
-            path: `./config/.env.${env === "production" ? "production" : "development"}`,
-        })
-    ]
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(s*)css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: `${__dirname}/dist`,
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new Dotenv({
+      path: `./config/.env.${env === 'production' ? 'production' : 'development'}`,
+    }),
+  ],
 };
