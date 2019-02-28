fa1add890a45d2848db8c03cb1b847dad31f586f
Kristof Soomers <kristof.soomers@bannerconnect.net>
2019-02-28 14:47:45 +0100

    The default reducer, to set an example

--- src/reducers/DefaultApp.js
+++ src/reducers/DefaultApp.js
@@ -1,26 +1,25 @@
 // Define the initial state
 const initialState = {
-    cover: "https://s3.amazonaws.com/titlepages.leanpub.com/reactjsforthevisuallearner/hero?1496374274",
-    title: "React.js for the Visual Learner",
-    author: "Mike Mangialardi",
-    link: "https://leanpub.com/reactjsforthevisuallearner"
+  cover: 'https://s3.amazonaws.com/titlepages.leanpub.com/reactjsforthevisuallearner/hero?1496374274',
+  title: 'React.js for the Visual Learner',
+  author: 'Mike Mangialardi',
+  link: 'https://leanpub.com/reactjsforthevisuallearner',
 };
 
-//define a reducer with an initialized state action
+// define a reducer with an initialized state action
 function DefaultApp(state = initialState, action) {
-    //eventually adding logic to handle create, update, and delete
-    switch(action.type) {
-        case 'ADD':
-            let incremented = Object.assign({}, state);
-            incremented.books.push(incremented.books.length);
-            return incremented;
-        case 'DELETE':
-            let decremented = Object.assign({}, state);
-            decremented.books.pop();
-            return decremented;
-        default:
-            return state
-    }
+  // eventually adding logic to handle create, update, and delete
+  const selectedObject = Object.assign({}, state);
+  switch (action.type) {
+    case 'ADD':
+      selectedObject.books.push(selectedObject.books.length);
+      return selectedObject;
+    case 'DELETE':
+      selectedObject.books.pop();
+      return selectedObject;
+    default:
+      return state;
+  }
 }
 
-export default DefaultApp
+export default DefaultApp;
